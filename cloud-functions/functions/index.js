d7289154c1975aedf7810be86db546526654ec2e
Ben Schoenfeld <ben.schoenfeld@portsolutionintegrators.com>
2020-12-09 14:03:53 -0500

    Update cloud function node and firebase versions

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -1,20 +1,3 @@
-/**
- * Copyright 2015 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-'use strict';
-
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
 // Configure the email transport using the default SMTP transport and a GMail account.
@@ -26,8 +9,7 @@ const mailTransport = nodemailer.createTransport(
     `smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
 
 // Sends an email confirmation when a user changes his mailing list subscription.
-exports.sendEmailConfirmation = functions.database.ref('/users/{uid}/email').onWrite(event => {
-    const snapshot = event.data;
+exports.sendEmailConfirmation = functions.database.ref('/users/{uid}/email').onCreate((snapshot, context) => {
     const email = snapshot.val();
 
     if(!email) return;
@@ -42,3 +24,11 @@ exports.sendEmailConfirmation = functions.database.ref('/users/{uid}/email').onW
         console.log('New user notification sent for ', email);
     });
 });
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
